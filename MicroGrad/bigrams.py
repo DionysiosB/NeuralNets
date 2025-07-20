17d23ff8348f1b809feb43bd6f6b6110e487d6bc
Dionysios Barmpoutis <barmpoutis@gmail.com>
2025-07-20 19:09:09 +0300

    Add sentinel, log likelihoods

--- MicroGrad/bigrams.py
+++ MicroGrad/bigrams.py
@@ -15,7 +15,7 @@ fs = sep + sep.join(words) + sep
 
 
 # Construct the 2D transition array
-mat = np.zeros((B + 1, B + 1))
+mat = np.ones((B + 1, B + 1))  #Ones to avoid any log infinities
 for p in range(1, len(fs)):
     prev = B if fs[p - 1] == sep else (ord(fs[p - 1]) - ord('a'))
     next = B if fs[p] == sep else (ord(fs[p]) - ord('a'))
@@ -29,14 +29,27 @@ plt.show()
 # Normalize to probabilities
 for row in range(B + 1):
     row_count = np.sum(mat[row])
-    for col in range(B + 1): mat[row][col] /= row_count
+    for col in range(B + 2): mat[row][col] /= row_count
 
-for _ in range(N):
+output = [''] * N
+nll = 0.0
+cnt = 0
+
+for p in range(N):
     asciilist = list()
     while True:
         prev = asciilist[-1] if len(asciilist) else B
         next = np.random.choice(range(B + 1), p=mat[prev])
+        nll -= np.log(mat[prev][next])
+        cnt += 1
         if next == B: break
         asciilist.append(next)
-    output = ''.join(map(chr, [int(ord('a') + x) for x in asciilist]))
-    print(output
+    output[p] = ''.join(map(chr, [int(ord('a') + x) for x in asciilist]))
+
+
+#Results
+nll /= cnt
+print(f"\n\nNegative Log Likelihood:{nll}\n")
+
+print("\nPredicted Words:\n")
+for x in output: print(x)
