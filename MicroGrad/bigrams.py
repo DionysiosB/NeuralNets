81705cafc7c197b3e5e61a3f74cd3328e15d0620
Dionysios Barmpoutis <barmpoutis@gmail.com>
2025-07-21 19:33:00 +0300

    Update bigrams.py

--- MicroGrad/bigrams.py
+++ MicroGrad/bigrams.py
@@ -15,7 +15,7 @@ fs = sep + sep.join(words) + sep
 
 
 # Construct the 2D transition array
-mat = np.ones((B + 1, B + 1))  #Ones to avoid any log infinities
+mat = np.ones((B + 1, B + 1))  #np.ones instead of np.zeros to avoid any log infinities
 for p in range(1, len(fs)):
     prev = B if fs[p - 1] == sep else (ord(fs[p - 1]) - ord('a'))
     next = B if fs[p] == sep else (ord(fs[p]) - ord('a'))
@@ -29,8 +29,23 @@ plt.show()
 # Normalize to probabilities
 for row in range(B + 1):
     row_count = np.sum(mat[row])
-    for col in range(B + 2): mat[row][col] /= row_count
+    for col in range(B + 1): mat[row][col] /= row_count
 
+
+# Calculate lower bound for negative log loss
+nll = 0.0
+for p in range(1, len(fs)):
+    prev = B if fs[p - 1] == sep else (ord(fs[p - 1]) - ord('a'))
+    next = B if fs[p] == sep else (ord(fs[p]) - ord('a'))
+    nll -= np.log(mat[prev][next])
+
+nll /= (len(fs) - 1)
+print(f"\n\nAverage Lower Bound for Negative Log Likelihood:{nll}\n")
+
+
+
+
+# Output N sampls and calculate log loss for the sample
 output = [''] * N
 nll = 0.0
 cnt = 0
@@ -52,4 +67,4 @@ nll /= cnt
 print(f"\n\nNegative Log Likelihood:{nll}\n")
 
 print("\nPredicted Words:\n")
-for x in output: print(x)
+for x in output: print(x) 
