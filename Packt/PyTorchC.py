215b06469a61a65f22add0573a8e0547c8e36d7a
Dionysios Barmpoutis <dbarmpoutis@gmail.com>
2025-08-30 10:12:38 -0500

    Create PyTorchC.py

---
+++ Packt/PyTorchC.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+cars_file = 'https://gist.githubusercontent.com/noamross/e5d3e859aa0c794be10b/raw/b999fb4425b54c63cab088c0ce2c0d6ce961a563/cars.csv'
+cars = pd.read_csv(cars_file)
+X = cars['wt'].values
+y = cars['mpg'].values
+
+X = torch.tensor(X, dtype = torch.float32).reshape(-1, 1)
+y = torch.tensor(y, dtype = torch.float32).reshape(-1, 1)
+
+class LinearRegressionTorch(nn.Module):
+
+    def __init__(self, fanin, fanout):
+        super().__init__()
+        self.layer = nn.Linear(fanin, fanout, dtype=torch.float32)
+
+    def forward(self, data):
+        return self.layer(data)
+
+
+    def fit(self, X, ytrue, num_epochs = 1, learning_rate = 0.01, batch_size = 32):
+        optimizer = torch.optim.SGD(self.layer.parameters(), lr = learning_rate)
+        loss_function = nn.MSELoss()
+
+        losses = [0] * num_epochs
+
+        for epoch in range(num_epochs):
+            optimizer.zero_grad()
+
+            idx = torch.randint(low=0, high=ytrue.size(0), size=(batch_size,))
+            Xb = X[idx]
+            yb = y[idx]
+
+            yhat = self.layer(Xb)
+            loss = loss_function(yhat, yb)
+            loss.backward()
+            optimizer.step()
+            losses[epoch] = loss.item()
+
+        print(f"Final Loss:{loss.item()}")
+        plt.plot(losses[100:])
+        plt.show()
+
+
+
+model = LinearRegressionTorch(1, 1)
+model.fit(X, y, num_epochs=1000, learning_rate=0.02)
+print(f"Weight:{model.layer.weight.item()} Bias:{model.layer.bias.item()}")
+
